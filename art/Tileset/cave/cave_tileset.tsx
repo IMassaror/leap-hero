c47609ceeccdce3000a627e307c387cf58fd4b88
JapaSsou13 <willgamesjapa@gmail.com>
2025-12-07 02:00:27 -0300

    new art branch added

---
+++ art/Tileset/cave/cave_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="cave_tileset" tilewidth="8" tileheight="8" tilecount="25" columns="5">
+ <image source="cave_tileset.png" width="40" height="40"/>
+</tileset>
---
+++ art/Tileset/snake/snake_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="snake_tileset" tilewidth="8" tileheight="8" tilecount="198" columns="18">
+ <image source="snaketileset.png" width="144" height="88"/>
+</tileset>
